20a4ac3f5b59a09a007be6519bf3b216ff80cd3e
Ognen Mladenovski <ognen.mlad@gmail.com>
2026-08-20 01:55:38 +0200

    Commit Day 3.5

--- src/Generics/ExamExercies/GenericFractionTest.java
+++ src/Generics/ExamExercies/GenericFractionTest.java
@@ -1,6 +1,49 @@
+//Zadacha 12/51
 package Generics.ExamExercies;
 
 import java.util.Scanner;
+
+
+class ZeroDenominatorException extends Exception {
+    ZeroDenominatorException(String message) {
+        super("Denominator cannot be zero");
+    }
+}
+
+class GenericFraction<T extends Number, U extends Number> {
+    private T numerator;
+    private U denominator;
+
+    public GenericFraction(T numerator, U denominator) throws ZeroDenominatorException {
+        if (denominator.doubleValue() == 0) {
+            throw new ZeroDenominatorException("Denominator cannot be zero");
+        }
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
+
+    public GenericFraction<Double, Double> add(GenericFraction<? extends Number, ? extends Number> gf) throws ZeroDenominatorException {
+        Double new_numerator = (this.numerator.doubleValue() * gf.denominator.doubleValue() + gf.numerator.doubleValue() * this.denominator.doubleValue());
+        Double new_denominator = (this.denominator.doubleValue() * gf.denominator.doubleValue());
+        GenericFraction<Double, Double> dropka = new GenericFraction<Double, Double>(new_numerator, new_denominator);
+        return dropka;
+    }
+
+    public double toDouble() {
+        return numerator.doubleValue() / denominator.doubleValue();
+    }
+
+    private long gcd(long a, long b) {
+        if (b == 0) return a;
+        return gcd(b, a % b);
+    }
+
+    @Override
+    public String toString() {
+        long g = gcd(Math.round(numerator.doubleValue()), Math.round(denominator.doubleValue()));
+        return String.format("%.2f / %.2f", numerator.doubleValue() / g, denominator.doubleValue() / g);
+    }
+}
 
 public class GenericFractionTest {
     public static void main(String[] args) {
@@ -20,7 +63,7 @@ public class GenericFractionTest {
             System.out.println(gfInt.add(gfFloat));
             System.out.println(gfDouble.add(gfInt));
             gfInt = new GenericFraction<Integer, Integer>(n3, 0);
-        } catch(ZeroDenominatorException e) {
+        } catch (ZeroDenominatorException e) {
             System.out.println(e.getMessage());
         }
 
